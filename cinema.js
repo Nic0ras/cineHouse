a2c5f574fe72d462da23599edab555e7eb48634e
Nic0ras <nicollai2006@gmail.com>
2023-05-11 21:42:24 -0300

    configuração do projeto cinema

---
+++ cinema.js
@@ -0,0 +1,9 @@
+const Filme = require('./filme')
+
+const cinema = 'DH - Movies';
+
+console.log(cinema)
+
+const catalogo = [];
+
+console.log(new Filme (1, 'Harry Potter', 90, [],2001, false));
---
+++ filme.js
@@ -0,0 +1,16 @@
+module.exports = function Filme(
+  codigo,
+  titulo,
+  duracao,
+  atores,
+  anoDeLancamento,
+  emCartaz
+) {
+  this.codigo = codigo;
+  this.titulo = titulo;
+  this.duracao = duracao;
+  this.atores = atores;
+  this.anoDeLancamento = anoDeLancamento;
+  this.emCartaz = emCartaz;
+  return //toda a função precisa de um retorno
+};
